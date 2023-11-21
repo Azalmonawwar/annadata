6eac35a4c3b7c545c74a957c9817cc414dcb6838
Rathin Gorai <rthnkg@gmail.com>
2023-11-21 20:47:12 +0530

    profile

--- src/app/(protected)/dash/profile/page.jsx
+++ src/app/(protected)/dash/profile/page.jsx
@@ -1,4 +1,3 @@
-
 import NavDash from '@/components/NavDash'
 import Sidebar from '@/components/Sidebar'
 
@@ -22,29 +21,26 @@ const Profile = () => {
           <div className="text-gray-700">
             <div className="grid text-sm md:grid-cols-2">
               <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">First Name</div>
+                <div className="px-4 py-2 font-semibold"> Name</div>
                 <div className="px-4 py-2">Jane</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Last Name</div>
-                <div className="px-4 py-2">Doe</div>
-              </div>
+              </div>  
               <div className="grid grid-cols-2">
                 <div className="px-4 py-2 font-semibold">Gender</div>
                 <div className="px-4 py-2">Female</div>
               </div>
               <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">Address</div>
+                <div className="px-4 py-2">Jamshedpur</div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="px-4 py-2 font-semibold">Pin Code</div>
+                <div className="px-4 py-2">832304</div>
+              </div>
+              <div className="grid grid-cols-2">
                 <div className="px-4 py-2 font-semibold">Contact No.</div>
                 <div className="px-4 py-2">+11 998001001</div>
               </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Current Address</div>
-                <div className="px-4 py-2">Beech Creek, PA, Pennsylvania</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Permanant Address</div>
-                <div className="px-4 py-2">Arlington Heights, IL, Illinois</div>
-              </div>
+              
               <div className="grid grid-cols-2">
                 <div className="px-4 py-2 font-semibold">Email.</div>
                 <div className="px-4 py-2">
@@ -53,46 +49,8 @@ const Profile = () => {
                   </a>
                 </div>
               </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Birthday</div>
-                <div className="px-4 py-2">Feb 06, 1998</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">First Name</div>
-                <div className="px-4 py-2">Jane</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Last Name</div>
-                <div className="px-4 py-2">Doe</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Gender</div>
-                <div className="px-4 py-2">Female</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Contact No.</div>
-                <div className="px-4 py-2">+11 998001001</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Current Address</div>
-                <div className="px-4 py-2">Beech Creek, PA, Pennsylvania</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Permanant Address</div>
-                <div className="px-4 py-2">Arlington Heights, IL, Illinois</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Email.</div>
-                <div className="px-4 py-2">
-                  <a className="text-blue-800" href="mailto:jane@example.com">
-                    jane@example.com
-                  </a>
-                </div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Birthday</div>
-                <div className="px-4 py-2">Feb 06, 1998</div>
-              </div>
+              
+              
             </div>
           </div>
           <button className="focus:shadow-outline hover:shadow-xs my-4 block w-full rounded-lg p-3 text-sm font-semibold text-blue-800 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none">
